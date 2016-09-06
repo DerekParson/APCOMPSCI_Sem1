25ffc39ac955abf4a8619fa2e473f84cc9115a06
DerekParsons <derixemail@gmail.com>
2016-09-06 12:15:47 -0700

    Addded Items

---
+++ APLesson_2/Lab_02.java
@@ -0,0 +1,20 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int first = 337;
+		int second = 1677;
+		int mult = first * second;
+		
+		System.out.println(first + " multiplied by " + second + " is " + mult);
+	
+		int name = Joe_Smith;
+		int addressNum = 1234;
+		int addressName = Easy_Street;
+		int city = Suburbia;
+		int zip = 12345;
+		
+		System.out.println(name+ addressNum + " " + addressName + city + zip);
+	}
+
+}
