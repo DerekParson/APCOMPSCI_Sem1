ff0cdf9adc02af87692d7385a31856f3340dc174
DerekParson <derixemail@gmail.com>
2016-10-20 12:15:25 -0700

    added lab 5.2

---
+++ APLesson_05/Lab_05_2.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class Lab_05_2
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Which class do you want to be? Mage or Warrior?");
+		String spec = kb.next();
+		if(spec.equals("Mage"))
+			System.out.println("You have picked Mage. Do you want a Wand of Frost or Flame?");
+			String wand = kb.next();
+			if (wand.equals("Frost"))
+			{	
+			System.out.println("You see a skeleton. Use your wand? Y/N");
+				String atkSkeleton = kb.next();
+				if(atkSkeleton.equals("Y"))
+					System.out.println("You froze the skeleton, but it didn't stop him from killing you. \nYou lose.");					
+				else if(atkSkeleton.equals("N"))
+					System.out.println("You chose to run from the skeleton, but you fell into a pit of spikes. \nYou lose");
+			}
+			else if (wand.equals("Flame"))
+			{	
+				System.out.println("You see a tapestry. Set it on fire to check for a secret door? Y/N");
+				String setFire = kb.next();
+				if(setFire.equals("Y"))
+					System.out.println("You burn the tapestry and see a chest of gold behind it. \nYou now have 500 gold coins. You win.");
+				else if(setFire.equals("N"))
+					System.out.println("You decide to forfeit the treasure and walk away, but a spear trap was concealed in the tapestry. \nYou lose.");
+			}	
+		else
+			System.out.println("You have picked Warrior. Do you want a Broadsword or a Dagger?");
+			String weapon = kb.next();
+			if(weapon.equals("Broadsword"))
+			{
+				System.out.println("You pick up the broadsword but it is too heavy. How many coins do you drop to empty your pack?");
+				int dropCoins = kb.nextInt();
+				if(dropCoins >= 50)
+					System.out.println("You drop enough coins to hold the broadsword. \nYou clear out the rest of the dungeon with ease. You win");
+				else
+					System.out.println("You haven't dropped enough coins and you quickly get exhausted and slain by a goblin.\nYou lose.");
+			}
+			else if (weapon.equals("Dagger"))
+			{
+				System.out.println("You pick up the dagger. Do you want to try to pick the lock on a chest? Y/N");
+				String pickLock = kb.next();
+				if(pickLock.equals("Y"))
+					System.out.println("While you are trying to pick the lock, a wraith sneaks up on you and absorbs your soul.\nYou lose.");
+				else if (pickLock.equals("N"))
+					System.out.println("You decide to leave without the treasure and make it out of the dungeon safely.\nYou win.");
+			}
+	}
+}
--- APLesson_05/Logical.java
+++ APLesson_05/Logical.java
@@ -1,3 +1,4 @@
+import java.util.Scanner
 public class Logical
 {
 	public static void main(String[]args)
@@ -6,6 +7,25 @@ public class Logical
 		boolean b = false;
 		
 		System.out.println(a && b);
+		System.out.println(a || b);
+		System.out.println(!(a && b));
+		
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Pick a number between 1 and 10");
+		int guess = kb.nextInt();
+		if(guess >=1 && guess<=10)
+		{
+			System.out.println("The number is right");
+		}
+		else
+		{
+			System.out.println("The number is wrong");
+			
+		}
+		
+		
+		
+		
 		
 	}
 }
