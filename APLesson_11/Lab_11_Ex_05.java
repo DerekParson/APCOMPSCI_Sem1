814efb0be23df2017652c6c4704d07a646b902fc
DerekParson <derixemail@gmail.com>
2017-01-26 11:01:48 -0800

    changed 11 ex 5

---
+++ APLesson_11/Lab_11_Ex_05.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Lab_11_Ex_05
+{
+	public static int bulletCount;
+	public static int clipsize = 16;
+	public static int shotCount;
+	public static int clip;
+	
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		bulletCount = 96;
+		shotCount = 0;
+		String[] = new String[clipsize];
+		resetclip();
+		while(bulletCount || shotCount > 0)
+		{
+			System.out.println("Action: ")
+			
+		}
+	}
+	public static int resetClip()
+	{
+		
+	}
+	public static int shoot()
+	{
+		
+	}
+	public static int reload()
+	{
+		
+	}
+	public static int printClip()
+	{
+		
+	}
+}
